76538d4cd7c39a505344f5d9d34487a614f6e497
Waseem khalil <waseemkhalilworl@gmail.com>
2024-05-29 21:29:02 -0700

    100% work ok

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const API_KEY = "cc1b2aeaf10558afead4dd361e05fd21";
 const form = document.querySelector("form");
 const search = document.querySelector("#search");
 const weather = document.querySelector("#weather");
+const btn = document.querySelector("#getWeatherBtn")
 // const API = `https://api.openweathermap.org/data/2.5/weather?
 // q=${city}&appid=${API_KEY}&units=metric`
 // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
 const getWeather = async (city) => {
   weather.innerHTML = `<h2> Loading... <h2>`;
+  btn.disabled = true; // disable button 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   const response = await fetch(url);
   const data = await response.json();
@@ -23,7 +25,7 @@ const showWeather = (data) => {
             <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
         </div>
         <div>
-            <h2>${data.main.temp} ℃</h2>
+          <h2>${data.main.temp} ℃</h2>
             <h4> ${data.weather[0].main} 
           
         </div>
